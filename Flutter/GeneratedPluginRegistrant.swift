4b592a9cb3eeae5a9043feffd4ec311696e90b2a
amantechhub <aman.maurya.profile@gmail.com>
2026-06-18 19:36:36 +0530

    Add files via upload
    Upload complete Flutter Smart Door Lock project files

---
+++ Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_tts
+import local_auth_darwin
+import mobile_scanner
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+}
